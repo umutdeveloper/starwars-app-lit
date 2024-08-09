1a1db6ff53f81264fd296199c595ae40ad0ced47
Umut ÇAKIR <umutcakirbm@gmail.com>
2024-08-09 14:18:39 +0300

    Configure Docker, compose file and nginx

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { Router } from '@vaadin/router';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 setBasePath(import.meta.env.PROD ? '/assets/shoelace' : '/node_modules/@shoelace-style/shoelace/dist');
 
+const BASE_URL = import.meta.env.VITE_BASE_HREF;
+
 import './index.css';
 import './app';
 import routes from './app.routes';
 
 const routerOutlet = document.querySelector('app-root');
-const router = new Router(routerOutlet);
+const router = new Router(routerOutlet, { baseUrl: BASE_URL });
 router.setRoutes(routes);
